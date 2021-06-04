e9f8d1570172a617a3427895bc771f9c0f2b5197
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2021-06-04 12:02:02 -0300

    geração de imagem e contêiner Docker para o serviço web e API REST

---
+++ 82-gerar-conteiner-docker-sklearn.sh
@@ -0,0 +1,4 @@
+sudo docker build -t appweb  -f docker/dockerfile-sklearn.txt .
+
+sudo docker images
+
---
+++ 83-rodar-servico-docker.sh
@@ -0,0 +1,8 @@
+# listar conteineres
+#sudo docker ps
+
+# rodar em modo interativo
+sudo docker run -it -p 80:5000 appweb
+
+# rodar em modo daemon
+#sudo docker run -d -p 80:5000 appweb
