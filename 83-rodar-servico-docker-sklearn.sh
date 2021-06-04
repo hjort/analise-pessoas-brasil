daf9fc27849d054d8945e0d208ef22aee3950550
Rodrigo Hjort <rodrigo.hjort@serpro.gov.br>
2021-06-04 15:18:15 -0300

    melhorias no script Docker

--- 83-rodar-servico-docker-sklearn.sh
+++ 83-rodar-servico-docker-sklearn.sh
@@ -1,8 +1,9 @@
 # listar conteineres
-#sudo docker ps
+sudo docker ps
 
 # rodar em modo interativo
 #sudo docker run --name app-pessoas -it -p 80:5000 app-pessoas
 
 # rodar em modo daemon
 sudo docker run --name app-pessoas -d -p 80:5000 app-pessoas
+sudo docker ps
